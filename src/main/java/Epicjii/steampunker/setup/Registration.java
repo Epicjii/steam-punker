358cac71ee89f1137fb715faf074ef5378f737e4
Epicjii <jacobliggett2000@gmail.com>
2022-02-13 00:24:19 -0500

    Update Registration.java

--- src/main/java/Epicjii/steampunker/setup/Registration.java
+++ src/main/java/Epicjii/steampunker/setup/Registration.java
@@ -1,9 +1,26 @@
 package Epicjii.steampunker.setup;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static Epicjii.steampunker.SteamPunker.MODID;
+
 public class Registration {
+
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    private static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, MODID);
 
 
     public static void init() {
-
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(bus);
+        ITEMS.register(bus);
+        FLUIDS.register(bus);
     }
 }
